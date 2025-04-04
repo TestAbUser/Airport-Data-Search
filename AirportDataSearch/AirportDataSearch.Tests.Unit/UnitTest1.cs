280a77ba5075e04f27db74756d9c13b8e1eb9ffd
Rashid Isaev <isarutul@yandex.ru>
2025-04-04 19:24:09 +0300

    Add IFileSystem interface

--- AirportDataSearch/AirportDataSearch.Tests.Unit/UnitTest1.cs
+++ AirportDataSearch/AirportDataSearch.Tests.Unit/UnitTest1.cs
@@ -1,10 +1,15 @@
-﻿namespace AirportDataSearch.Tests.Unit
+﻿using Moq;
+
+namespace AirportDataSearch.Tests.Unit
 {
     public class UnitTest1
     {
         [Fact]
         public void Test1()
         {
+            var file = new Mock<IFileSystem>();
+
+            //var sut = new Search(file.Object);
 
         }
     }
