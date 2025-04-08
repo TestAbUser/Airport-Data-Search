43579b51064d59070667ebd5624dde8078671cf3
Rashid Isaev <isarutul@yandex.ru>
2025-04-08 03:37:00 +0300

    Implement sorting depending on column type

--- AirportDataSearch/AirportDataSearch/Program.cs
+++ AirportDataSearch/AirportDataSearch/Program.cs
@@ -6,47 +6,43 @@ namespace AirportDataSearch
 {
     public class Program
     {
-        private static readonly Searcher? _searcher=  new Searcher() ;
+        private static readonly Searcher? _searcher = new Searcher();
         private static IFileSystem? _file = new FileSystem();
-       private static Stopwatch sw = Stopwatch.StartNew();
+        private static Stopwatch sw = Stopwatch.StartNew();
         static string path = Path.Combine(Environment.CurrentDirectory, "airports.dat");
 
         public Program()
         { }
-        
-        public static void Main(string[]args)
+
+        public static void Main(string[] args)
         {
             Console.OutputEncoding = System.Text.Encoding.Unicode;
-            
+
             //To Do: check that command line parameter not bigger than number of columns
-            int commandLineParameter= int.Parse(args[0]);
+            int commandLineParameter = int.Parse(args[0]);
             _searcher!.ColumnIndex = commandLineParameter;
-          //  _searcher.ParseFile(GetFileContent());
-           // Searcher searcher = new() { ColumnIndex = commandLineParameter};
 
-           // IFileSystem fileSystem = new FileSystem();
-           // var lines = fileSystem.ReadLines(path);
 
-       Program program = new Program(); 
-            
-            
-         program.StartSearch(GetSearchInput());
+            Program program = new Program();
+
+
+            program.StartSearch(GetSearchInput());
         }
 
         private static string[] GetFileContent()
         {
-           return  _file!.ReadLines(path);
+            return _file!.ReadLines(path);
         }
 
-        IOrderedEnumerable<IGrouping<string, string>> file = 
+        IOrderedEnumerable<IGrouping<string, string>> file =
             _searcher!.ParseFile(GetFileContent());
 
-       public void StartSearch(string searchedString)
+        public void StartSearch(string searchedString)
         {
             while (searchedString != "!quit")
             {
                 sw.Start();
-                var searchResult =_searcher!.Find(searchedString, ref file);
+                var searchResult = _searcher!.Find(searchedString, ref file);
                 sw.Stop();
                 IView displayer = new Displayer();
                 displayer.Show(searchResult, sw);
--- AirportDataSearch/AirportDataSearch/Searcher.cs
+++ AirportDataSearch/AirportDataSearch/Searcher.cs
@@ -7,37 +7,43 @@ namespace AirportDataSearch
     {
         public int ColumnIndex { get; set; }
         public IEnumerable<IGrouping<string, string>> Find(
-            string searchString, 
+            string searchString,
             ref IOrderedEnumerable<IGrouping<string, string>> fileContent/*string[] fileContent*/)
         {
-            var parsedFile = fileContent;//ParseFile(fileContent);
+            var parsedFile = fileContent;
             var result = parsedFile.Where(
-                x => Regex.IsMatch(x.Key, "^\"?" + 
+                x => Regex.IsMatch(x.Key, "^\"?" +
                 Regex.Escape(searchString), RegexOptions.IgnoreCase));
             return result;
         }
 
         public IOrderedEnumerable<IGrouping<string, string>> ParseFile(string[] fileContent)
         {
-            Regex regex = new("^[-+]?[0-9]*\\.[0-9]+$");
+            Regex regex = new(@"^[-+]?[0-9]*\.[0-9]+$");
             Lookup<string, string> result =
                 (Lookup<string, string>)fileContent.ToLookup(x =>
-                x.Split(',')[ColumnIndex - 1], x => x);
+                x.Split(',')[ColumnIndex - 1], x => x);// comma is not enough
 
-            if(result.Any(x=>x.Key.StartsWith('"')&& x.Key.EndsWith('"')))
+
+            if (result.Any(x => regex.IsMatch(x.Key)))
             {
-                var sortedResult = result.OrderBy(x => x.Key);
-                return sortedResult;
+                var sorted = 
+            result.Where(x => x.Key.All(y => char.IsDigit(y) || y == '.' || y == '-'))
+            .OrderBy(z => double.Parse(z.Key))
+            .Union(result.Where(d => d.Key.Any(f => char.IsLetter(f))))
+            .OrderBy(x => x.Key);
+                return sorted;
             }
-               
-            else if (result.Any(x =>regex.IsMatch(x.Key)))
+
+            else if (result.Any(x => x.Key.All(x => char.IsDigit(x) || x == '-')))
             {
-                var sorted = result.OrderBy(x => double.TryParse(x.Key, out double _));
-            return sorted;
+                return result.OrderBy(x => int.Parse(x.Key))
+                             .Union(result.Where(d => d.Key.Any(f => char.IsLetter(f))))
+                             .OrderBy(x => x.Key);
             }
-                
-           else return result.OrderBy(x => int.Parse(x.Key));
-               // return result;
+
+            else
+                return result.OrderBy(x => x.Key);
         }
     }
 }
